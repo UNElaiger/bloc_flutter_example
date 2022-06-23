bb2f9a9386f867c20b67c1d9173e8615d0445780
UNElaiger <patrooldiman@mail.ru>
2022-06-23 17:28:19 +0300

    create

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'color_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BLoC with flutter_bloc',
+      home: BlocProvider(
+        create: (context) => ColorBloc(),
+        child: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
+    ColorBloc bloc_obj;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BLoC with flutter_bloc'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: BlocBuilder<ColorBloc, Color>(
+          builder: (context, currentColor) => AnimatedContainer(
+            height: 100,
+            width: 100,
+            color: currentColor,
+            duration: const Duration(milliseconds: 500),
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              bloc.add(RedColorEvent());
+            },
+            backgroundColor: Colors.red,
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () {
+              bloc.add(GreenColorEvent());
+            },
+            backgroundColor: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+}
